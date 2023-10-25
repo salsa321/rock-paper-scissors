8978624a38c8e712853b01368b66a64a70ab95df
salsa321 <123456789+odin@users.noreply.github.com>
2023-10-25 15:05:14 +0100

    Create function to support functional game of RPS
    A function has been created to support a fully functional game of RPS
    using the users input as well as computers random response. The outcome
    also comes with a statement highlighting if the user won, lost or drew.

--- javascript.js
+++ javascript.js
@@ -10,3 +10,35 @@ function getComputerChoice () {
         return ('Scissors');
     }
 }
+
+function singleRoundOfRPS (playerSelection, computerSelection = getComputerChoice()) {
+    let userChoice = playerSelection.toUpperCase();
+    /*let computerSelection = getComputerChoice();*/
+    if (userChoice === 'ROCK' && computerSelection === 'Scissors') {
+        return 'You win! Rock beats Scissors';
+    }
+    else if (userChoice === 'ROCK' && computerSelection === 'Paper') {
+        return 'You lose! Paper beats Rock';
+    }
+    /*else if (userChoice === 'ROCK' && computerSelection === 'Rock') {
+        return 'Its a draw!! Play Again!';
+    }*/
+    else if (userChoice === 'PAPER' && computerSelection === 'Rock') {
+        return 'You win! Paper beats Rock';
+    }
+    else if (userChoice === 'PAPER' && computerSelection === 'Scissors') {
+        return 'You lose! Scissors beats Paper';
+    }
+    /*else if (userChoice === 'PAPER' && computerSelection === 'Paper') {
+        return 'Its a draw'
+    }*/
+    else if (userChoice === 'SCISSORS' && computerSelection === 'Paper') {
+        return 'You win! Scissors beats Paper';
+    }
+    else if (userChoice === 'SCISSORS' && computerSelection === 'Rock') {
+        return 'You lose! Rock beats Scissors';
+    }
+    else {
+        return 'Its a draw!! Play again!';
+    }
+}
