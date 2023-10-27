9aa29b69c67147c607ddbc56435289fb6cab6fe2
salsa321 <123456789+odin@users.noreply.github.com>
2023-10-27 15:08:09 +0100

    Alter singleRoundOfRPS parameter to include prompt
    Before this, the singleRoundOfRPS function did not contain the prompt
    as a parameter. This produced errors when it came to trying to get the
    game function to work as this required a prompt box with a different
    input, each, for 5 different games instead of just one. The game
    function calls upon the singleRoundOfRPS function.

--- javascript.js
+++ javascript.js
@@ -12,27 +12,26 @@ function getComputerChoice () {
 }
 
 function singleRoundOfRPS (playerSelection, computerSelection = getComputerChoice()) {
-    let userChoice = playerSelection.toUpperCase();
-    /*let computerSelection = getComputerChoice();*/
-    if (userChoice === 'ROCK' && computerSelection === 'Scissors') {
+    playerSelection = prompt('Enter Rock, Paper or Scissors:', '').toUpperCase();
+    if (playerSelection === 'ROCK' && computerSelection === 'Scissors') {
         return 'You win! Rock beats Scissors';
     }
-    else if (userChoice === 'ROCK' && computerSelection === 'Paper') {
+    else if (playerSelection === 'ROCK' && computerSelection === 'Paper') {
         return 'You lose! Paper beats Rock';
     }
-    else if (userChoice === 'PAPER' && computerSelection === 'Rock') {
+    else if (playerSelection === 'PAPER' && computerSelection === 'Rock') {
         return 'You win! Paper beats Rock';
     }
-    else if (userChoice === 'PAPER' && computerSelection === 'Scissors') {
+    else if (playerSelection === 'PAPER' && computerSelection === 'Scissors') {
         return 'You lose! Scissors beats Paper';
     }
-    else if (userChoice === 'SCISSORS' && computerSelection === 'Paper') {
+    else if (playerSelection === 'SCISSORS' && computerSelection === 'Paper') {
         return 'You win! Scissors beats Paper';
     }
-    else if (userChoice === 'SCISSORS' && computerSelection === 'Rock') {
+    else if (playerSelection === 'SCISSORS' && computerSelection === 'Rock') {
         return 'You lose! Rock beats Scissors';
     }
-    else if (userChoice !== 'ROCK' && userChoice !== 'PAPER' && userChoice !== 'SCISSORS') {
+    else if (playerSelection !== 'ROCK' && playerSelection !== 'PAPER' && playerSelection !== 'SCISSORS') {
         return 'Please enter either Rock, Paper or Scissors';
     }
     else {
@@ -40,14 +39,46 @@ function singleRoundOfRPS (playerSelection, computerSelection = getComputerChoic
     }
 }
 
-/*function game() {
+function game() {
     for (let i = 0; i < 5; i++) {
-        console.log(prompt('Enter Rock, Paper or Scissors:', '');
-        
-            singleRoundOfRPS(prompt('Enter Rock, Paper or Scissors:', ''), computerSelection));
+        let win = 0;
+        let lose = 0;
+        let draw = 0;
+        console.log (singleRoundOfRPS());
+        if (singleRoundOfRPS() === 'You win! Rock beats Scissors'
+        || singleRoundOfRPS() === 'You win! Paper beats Rock' 
+        || singleRoundOfRPS() === 'You win! Scissors beats Paper') {
+            win = ++win;
+        }
+        else if (singleRoundOfRPS() === 'You lose! Paper beats Rock'
+        || singleRoundOfRPS() === 'You lose! Scissors beats Paper' 
+        || singleRoundOfRPS() === 'You lose! Rock beats Scissors') {
+            lose = ++lose;
+        }
+        else if (singleRoundOfRPS() === 'Its a draw!! Play again!') {
+            draw = ++draw;
+        }
+        (function () {
+            if (i === 4 && win > 2) {
+                return "Congratulations! You've won the game.";
+            }
+            else if (i === 4 && lose > 2) {
+                return "Unlucky :( - You've lost, try again.";
+            }
+            else if (i === 4 && draw > 2) {
+                return "It's a draw! - play again.";
+            }
+        });    
+        /*if (win > 2) {
+            return "Congratulations! You've won the game";
+        }
+        else if (lose > 2) {
+            return "Unlucky :( - You've lost, try again";
+        }
+        else {
+            return "It's a draw! - Play again.";
+        }*/
     }
 }
 
-/*console.log(game());*/
-
-console.log (singleRoundOfRPS(prompt('Enter Rock, Paper or Scissors:', '')));
+console.log();
