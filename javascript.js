f93923b536cd7e531f6b206dff55a953ac393cc4
salsa321 <123456789+odin@users.noreply.github.com>
2023-11-05 20:20:34 +0000

    Fix function to run RPS 5 times and report result
    Previously an incorrect attempt of using for loops was present. Instead
    of using for loops, the function is mow called upon 5 separate times.
    The result of each separate game adds 1 to the win/lose/draw variable.
    If either the win or lose variable is > 2 it reports the outcome with a
    corresponding win or lose statement otherwise it returns a draw
    statement

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let win = 0;
+let lose = 0;
+let draw = 0;
+
 function getComputerChoice () {
     let random = Math.random();
     if (random <= 0.3333) {
@@ -14,21 +18,27 @@ function getComputerChoice () {
 function singleRoundOfRPS (playerSelection, computerSelection = getComputerChoice()) {
     playerSelection = prompt('Enter Rock, Paper or Scissors:', '').toUpperCase();
     if (playerSelection === 'ROCK' && computerSelection === 'Scissors') {
-        return 'You win! Rock beats Scissors';
+        win++;
+        return  'You win! Rock beats Scissors';
     }
     else if (playerSelection === 'ROCK' && computerSelection === 'Paper') {
+        lose++;
         return 'You lose! Paper beats Rock';
     }
     else if (playerSelection === 'PAPER' && computerSelection === 'Rock') {
+        win++;
         return 'You win! Paper beats Rock';
     }
     else if (playerSelection === 'PAPER' && computerSelection === 'Scissors') {
+        lose++;
         return 'You lose! Scissors beats Paper';
     }
     else if (playerSelection === 'SCISSORS' && computerSelection === 'Paper') {
+        win++;
         return 'You win! Scissors beats Paper';
     }
     else if (playerSelection === 'SCISSORS' && computerSelection === 'Rock') {
+        lose++;
         return 'You lose! Rock beats Scissors';
     }
     else if (playerSelection !== 'ROCK' && playerSelection !== 'PAPER' && playerSelection !== 'SCISSORS') {
@@ -40,25 +50,37 @@ function singleRoundOfRPS (playerSelection, computerSelection = getComputerChoic
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        let win = 0;
-        let lose = 0;
-        let draw = 0;
-        console.log (singleRoundOfRPS());
-        if (singleRoundOfRPS() === 'You win! Rock beats Scissors'
-        || singleRoundOfRPS() === 'You win! Paper beats Rock' 
-        || singleRoundOfRPS() === 'You win! Scissors beats Paper') {
+    singleRoundOfRPS();
+    singleRoundOfRPS();
+    singleRoundOfRPS();
+    singleRoundOfRPS();
+    singleRoundOfRPS();
+    /*for (let i = 0; i < 5; i++) {
+        console.log(singleGame);
+    }*/
+    if (win > 2) {
+        return "Congratulations! You have won the game!";
+    }
+    else if (lose > 2) {
+        return "Unlucky - You have lost...Try again.";
+    }
+    else {
+        return "It's a draw! Play again.";
+    }
+        /*if (singleGame === 'You win! Rock beats Scissors'
+        || singleGame === 'You win! Paper beats Rock' 
+        || singleGame === 'You win! Scissors beats Paper') {
             win = ++win;
         }
-        else if (singleRoundOfRPS() === 'You lose! Paper beats Rock'
-        || singleRoundOfRPS() === 'You lose! Scissors beats Paper' 
-        || singleRoundOfRPS() === 'You lose! Rock beats Scissors') {
+        else if (singleGame === 'You lose! Paper beats Rock'
+        || singleGame === 'You lose! Scissors beats Paper' 
+        || singleGame === 'You lose! Rock beats Scissors') {
             lose = ++lose;
         }
-        else if (singleRoundOfRPS() === 'Its a draw!! Play again!') {
+        else if (singleGame === 'Its a draw!! Play again!') {
             draw = ++draw;
-        }
-        (function () {
+            
+            (function () {
             if (i === 4 && win > 2) {
                 return "Congratulations! You've won the game.";
             }
@@ -68,17 +90,9 @@ function game() {
             else if (i === 4 && draw > 2) {
                 return "It's a draw! - play again.";
             }
-        });    
-        /*if (win > 2) {
-            return "Congratulations! You've won the game";
-        }
-        else if (lose > 2) {
-            return "Unlucky :( - You've lost, try again";
-        }
-        else {
-            return "It's a draw! - Play again.";
-        }*/
-    }
+        });*/ 
+           
+       
 }
 
-console.log();
+console.log(game());
